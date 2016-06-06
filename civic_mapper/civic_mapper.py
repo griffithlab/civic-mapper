40e899777f0aa743c7ddf25fa2862b8f92739342
Cody Ramirez <cramirez@genome.wustl.edu>
2016-06-06 14:06:43 -0500

    Deleting commented out code that was no longer needed

--- civic_mapper/civic_mapper.py
+++ civic_mapper/civic_mapper.py
@@ -61,12 +61,13 @@ for chromosome_number in range(1,23):
 	civic_variants_with_coordinates_dictionary[str(chromosome_number)] = []
 
 raw_civic_variants_with_coordinates_list = []
-# The civic_variants_without_coordinates_dictionary will hold all variants based on chromosome location (dictionary key) and exist within a list
 civic_variants_without_coordinates_dictionary = {}
+
 # Directly requesting a list of all variants from the CIViC API in JSON format
 variants = requests.get('https://civic.genome.wustl.edu/api/variants?count=10000').json()['records']
 
 # Iterating through the entire list of variants and parsing out variants without coordinates, while keeping count of all variants
+####################
 for current_variant in range(0, len(variants)):
 	#####Currently filtering out variants without representative trascripts because they most likely won't have coordinates
 	"""NOTE: Variants that are [FUSIONS, EXPRESSION, UNDEREXPRESSION, OVEREXPRESSION, AMPLIFICATION, LOSS] are also being removed currently to be dealt with later COME BACK TO HANDLE THESE VARIANTS FOR MAPPING AND REPORTATION."""
@@ -79,6 +80,7 @@ for current_variant in range(0, len(variants)):
 			civic_variants_without_coordinates_dictionary[gene_name] = [variants[current_variant]]
 	else:
 		raw_civic_variants_with_coordinates_list.append(variants[current_variant])
+####################
 
 # Sorting all variants by stop coordinate in ascending order (least to greatest) 
 raw_civic_variants_with_coordinates_list = sorted(raw_civic_variants_with_coordinates_list, key = lambda k: int(k['coordinates']['stop']))
@@ -91,21 +93,7 @@ for var in raw_civic_variants_with_coordinates_list:
 	# Inserting variants into civic_variants_with_coordinates_dictionary based on the index_location to maintain a sorted list
 	civic_variants_with_coordinates_dictionary[chr_key].insert(index_location, var)
 
-"""
-for chromo in civic_variants_with_coordinates_dictionary:
-	print('\n' + str(chromo))
 
-	for var in civic_variants_with_coordinates_dictionary[chromo]:
-		if int(var['coordinates']['start']) == int(var['coordinates']['stop']):
-			print( var['entrez_name'] + ':p.' + var['name'] + '\t' + var['coordinates']['start'])
-		else:
-			print(var['entrez_name'] + ':p.' + var['name'] + '\t' + var['coordinates']['start'] + '\t' + var['coordinates']['stop'])
-
-		#print(variants[current_variant]['entrez_name'] + ':p.' + variants[current_variant]['name'])
-##################################################
-#Extracts needed information from a transvar output file to map variants to CIViC and report findings
-##################################################
-"""
 for line in transvar_output:
 
 	line_list = line.strip().split()
